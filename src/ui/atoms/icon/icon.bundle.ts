70022ca22521ba78c21b6d8c66a60065e4860da7
Johan Ezequiel Sierra Linares <johanse.linares@gmail.com>
2022-09-26 10:46:15 -0400

    Added footer component.

--- src/ui/atoms/icon/icon.bundle.ts
+++ src/ui/atoms/icon/icon.bundle.ts
@@ -1,5 +1,6 @@
 import { FiUser } from "react-icons/fi";
 import { CgLock } from "react-icons/cg";
+import { BiCopyright } from "react-icons/bi";
 import { HiArrowRight } from "react-icons/hi";
 import { RiQuestionLine } from "react-icons/ri";
 import { FaDiscord, FaTwitter, FaInstagram } from "react-icons/fa";
@@ -10,6 +11,7 @@ const bundle = {
   discord: FaDiscord,
   twitter: FaTwitter,
   instagram: FaInstagram,
+  copyright: BiCopyright,
   "question-line": RiQuestionLine,
   "arrow-right": HiArrowRight,
 };
---
+++ src/ui/organisms/footer/footer.tsx
@@ -0,0 +1,40 @@
+import { TextIcon } from "@molecules";
+import { Box, Container, Divider, Flex, Icon, Text } from "@atoms";
+
+const IconTextSocial = () => (
+  <Flex sx={{ alignItems: "center" }}>
+    <Icon name="discord" />
+    <Text variant="footerText" ml={2}>
+      Discord
+    </Text>
+  </Flex>
+);
+
+const DividerVertical = () => <Divider mx={3} vertical />;
+
+const Footer = () => {
+  return (
+    <Box py={3} px={3} bg="secondary">
+      <Container sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Flex>
+          <TextIcon
+            text="Copyright 2022"
+            icon="copyright"
+            textProps={{ variant: "footerText" }}
+          />
+          <Text variant="footerText">Todos los derechos reservados</Text>
+        </Flex>
+
+        <Flex sx={{ alignItems: "center" }}>
+          <IconTextSocial />
+          <DividerVertical />
+          <IconTextSocial />
+          <DividerVertical />
+          <IconTextSocial />
+        </Flex>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
