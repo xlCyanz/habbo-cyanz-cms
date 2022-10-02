8c30a9340e978972f414623d03daa73251b4d772
Johan Ezequiel Sierra Linares <johanse.linares@gmail.com>
2022-10-01 20:50:56 -0400

    Update button component.

--- src/ui/atoms/button/button.tsx
+++ src/ui/atoms/button/button.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button, ButtonProps as ButtonPropsUI } from "theme-ui";
 
 import { SizeVariant } from "@types";
+import { StylesUtils } from "@utils";
 
 import { styles, styleSize } from "./button.styles";
 
@@ -9,8 +10,13 @@ export type ButtonProps = ButtonPropsUI & {
   size?: SizeVariant;
 };
 
-const ButtonWrapper = ({ size = "medium", ...props }: ButtonProps) => {
-  return <Button sx={{ ...styles.default, ...styleSize[size] }} {...props} />;
+const ButtonWrapper = ({ size = "medium", sx, ...props }: ButtonProps) => {
+  return (
+    <Button
+      sx={StylesUtils.compose(sx, { ...styles.default, ...styleSize[size] })}
+      {...props}
+    />
+  );
 };
 
 export default ButtonWrapper;
