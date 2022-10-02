d20503d74ab514b1e0cbf4b13c297cc4d0d95991
Johan Ezequiel Sierra Linares <johanse.linares@gmail.com>
2022-10-01 21:16:52 -0400

    Update footer component.

--- src/ui/organisms/footer/footer.tsx
+++ src/ui/organisms/footer/footer.tsx
@@ -17,7 +17,7 @@ const Footer = () => {
     <Box py={3} px={3} bg="secondary">
       <Container sx={{ display: "flex", justifyContent: "space-between" }}>
         <Flex sx={{ alignItems: "center" }}>
-          <TextIcon text="Copyright 2022" icon="copyright" />
+          <TextIcon text="Copyright 2022" iconName="copyright" />
           <Text variant="footerText">Ruby Hotel</Text>
           <Text variant="footerText">Todos los derechos reservados</Text>
         </Flex>
