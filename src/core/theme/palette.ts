d18b5aceb16d307921aa76efea72ac4f04b05cbe
Johan Ezequiel Sierra Linares <johanse.linares@gmail.com>
2022-10-02 20:43:56 -0400

    Removed unused colors.

--- src/core/theme/palette.ts
+++ src/core/theme/palette.ts
@@ -3,8 +3,6 @@ export default {
   vividGamboge: "#F90",
   chineseYellow: "#ffae00",
   eerieBlack: "#181818",
-  raisinBlack: "#222222",
-  onyx: "#383838",
   darkGunmetal: "#211F31",
   yankeesBlue: "#242438",
   independence: "#48446A",
